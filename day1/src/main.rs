14cfbe0ba3a092187c415368d57eed1a686d3987
Matt Stone <matthew.stone12@gmail.com>
2022-12-01 20:43:03 -0800

    refactor - extract total parsing and run both parts

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,36 +1,19 @@
 use std::io;
+use std::io::BufRead;
+// use std::io::BufReader;
 
-// fn part1() {
-//     let mut max_total: u32 = 0;
-//     let mut curr_total: u32 = 0;
-
-//     for line in io::stdin().lines() {
-//         let calories = line.expect("Failed to read");
-
-//         // Empty lines denote breaks between "elves"
-//         if calories.is_empty() {
-//             if curr_total > max_total {
-//                 max_total = curr_total;
-//             }
-//             curr_total = 0;
-//         // Add calories to current elf
-//         } else {
-//             let calories: u32 = match calories.trim().parse() {
-//                 Ok(num) => num,
-//                 Err(_) => continue,
-//             };
-//             curr_total += calories;
-//         }
-//     }
-
-//     println!("Max calories: {max_total}");
-// }
-
-fn main() {
+/// Parse calorie totals from input files.
+///
+/// Input files are formatted with calories listed on consecutive lines, with
+/// blank lines separating inventories of different elves.
+///
+/// The total calories carried by each elf are stored and returned in a vector.
+///
+fn parse_totals<R: BufRead>(reader: &mut R) -> Vec<u32> {
     let mut totals: Vec<u32> = Vec::new();
-    let mut curr_total = 0;
+    let mut curr_total: u32 = 0;
 
-    for line in io::stdin().lines() {
+    for line in reader.lines() {
         let calories = line.expect("Failed to read");
 
         // Empty lines denote breaks between "elves"
@@ -47,16 +30,27 @@ fn main() {
         }
     }
 
+    totals
+}
+
+fn part1(totals: &Vec<u32>) {
+    println!("Part 1: {}", totals.iter().max().unwrap());
+}
+
+fn part2(totals: &mut Vec<u32>) {
     totals.sort();
     totals.reverse();
 
     let total: u32 = totals[..3].iter().sum();
 
-    // let max = totals[0:2].sum();
-    // let max2 = totals[1];
+    println!("Part 2: {}", total);
+}
 
-    println!("{:?}", &totals[..3]);
-    println!("{:?}", total);
+fn main() {
+    // let mut reader = BufReader::new(io::stdin());
+    let mut reader = io::stdin().lock(); // StdinLock implements BufRead
+    let mut totals = parse_totals(&mut reader);
 
-    // println!("{totals[0]}, {max2}");
+    part1(&totals);
+    part2(&mut totals);
 }
