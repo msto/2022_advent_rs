bfa4fc8fb71a1acd0f61d70e782e804ccc8a0a98
Matt Stone <matthew.stone12@gmail.com>
2023-03-29 19:12:24 -0700

    add: move parsing

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -1,4 +1,4 @@
-fn parse_crates(line: &str) -> Vec<Option<char>> {
+pub fn parse_crates(line: &str) -> Vec<Option<char>> {
     line.chars()
         .collect::<Vec<char>>()
         .chunks(4)
@@ -10,6 +10,27 @@ fn parse_crates(line: &str) -> Vec<Option<char>> {
             }
         })
         .collect()
+}
+
+pub struct Move {
+    n: usize,
+    src: usize,
+    dst: usize,
+}
+
+pub fn parse_move(line: &str) -> Move {
+    let data: Vec<usize> = line
+        .split_whitespace()
+        .enumerate()
+        .filter(|(i, _)| i % 2 == 1)
+        .filter_map(|(_, x)| x.parse::<usize>().ok())
+        .collect();
+
+    Move {
+        n: data[0],
+        src: data[1],
+        dst: data[2],
+    }
 }
 
 #[cfg(test)]
@@ -26,5 +47,15 @@ mod tests {
         assert_eq!(crates[1], Some('B'));
         assert_eq!(crates[2], None);
         assert_eq!(crates[3], Some('D'));
+    }
+
+    #[test]
+    fn test_parse_move() {
+        let line = "move 1 from 3 to 5\n";
+        let mv = parse_move(line);
+
+        assert_eq!(mv.n, 1);
+        assert_eq!(mv.src, 3);
+        assert_eq!(mv.dst, 5);
     }
 }
