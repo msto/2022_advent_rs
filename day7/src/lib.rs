983cc06aace2d7f91a5d77e5f6223958d0db1ac9
Matt Stone <matthew.stone12@gmail.com>
2023-03-30 10:49:05 -0700

    add: node parsing

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -1,19 +1,43 @@
 use clap::Parser;
 use std::{
+    collections::HashMap,
     error::Error,
     fs::File,
     io::{self, BufRead, BufReader},
+    rc::Rc,
 };
 
-enum FsEntry {
+enum FsNode {
     Dir {
         name: String,
-        contents: Vec<FsEntry>,
+        contents: HashMap<String, Rc<FsNode>>,
+        parent: Option<Rc<FsNode>>,
     },
     File {
         name: String,
         size: usize,
+        parent: Option<Rc<FsNode>>,
     },
+}
+
+fn parse_node(line: &str, curr_dir: Option<Rc<FsNode>>) -> FsNode {
+    let mut data = line.split_whitespace();
+    let dtype = data.next().unwrap();
+    let name = data.next().unwrap();
+
+    if line.starts_with("dir ") {
+        FsNode::Dir {
+            name: name.to_string(),
+            contents: HashMap::new(),
+            parent: curr_dir,
+        }
+    } else {
+        FsNode::File {
+            name: name.to_string(),
+            size: dtype.parse::<usize>().unwrap(),
+            parent: curr_dir,
+        }
+    }
 }
 
 #[derive(Parser, Debug)]
@@ -33,8 +57,36 @@ pub fn get_args() -> Result<Args, Box<dyn Error>> {
 
 pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
     let fin = open(&args.fin)?;
+    let mut lines = fin.lines().filter_map(|x| x.ok());
 
-    for line in fin.lines() {}
+    // skip cd to root
+    lines.next();
+    let mut root = FsNode::Dir {
+        name: "/".to_string(),
+        contents: HashMap::new(),
+        parent: None,
+    };
+
+    let mut curr_dir = Rc::new(root);
+
+    for line in lines {
+        if line.starts_with("$") {
+            continue;
+        }
+
+        let node = parse_node(&line, Some(Rc::clone(&curr_dir)));
+
+        // if line.starts_with("$ cd") {
+        //     let name = line.trim_start_matches("$ cd ");
+        //     curr_dir = FsEntry::Dir {
+        //         name: name.to_string(),
+        //         contents: HashMap::new(),
+        //     };
+        //     curr_tree.push(curr_dir);
+        // } else if line.starts_with("$ ls") {
+        // } else {
+        // }
+    }
 
     Ok(())
 }
@@ -45,5 +97,49 @@ fn open(filename: &str) -> Result<Box<dyn BufRead>, Box<dyn Error>> {
         _ => Ok(Box::new(BufReader::new(
             File::open(filename).map_err(|e| format!("{}: {}", filename, e))?,
         ))),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::matches;
+
+    #[test]
+    fn test_parse_node_dir() {
+        let line = "dir abc";
+        let node = parse_node(&line, None);
+
+        assert!(matches!(node, FsNode::Dir { .. }));
+
+        match node {
+            FsNode::Dir {
+                name,
+                contents,
+                parent,
+            } => {
+                assert_eq!(name, "abc");
+                assert_eq!(contents.len(), 0);
+                assert!(parent.is_none());
+            }
+            FsNode::File { .. } => {}
+        };
+    }
+
+    #[test]
+    fn test_parse_node_file() {
+        let line = "123 def";
+        let node = parse_node(&line, None);
+
+        assert!(matches!(node, FsNode::File { .. }));
+
+        match node {
+            FsNode::File { name, size, parent } => {
+                assert_eq!(name, "def");
+                assert_eq!(size, 123);
+                assert!(parent.is_none());
+            }
+            FsNode::Dir { .. } => {}
+        };
     }
 }
