dd8246d170668a00c1d3b530b6c442a2ab55d8e0
Matt Stone <matthew.stone12@gmail.com>
2023-04-05 17:14:20 -0700

    add: working find_common_character and first attempt at solution with itertools::chunks

--- day3/part2/src/main.rs
+++ day3/part2/src/main.rs
@@ -1,6 +1,8 @@
-use std::collections::HashSet;
-use std::io;
-use std::io::BufRead;
+use itertools::Itertools;
+use std::{
+    collections::HashSet,
+    io::{self, BufRead},
+};
 
 /// Compute "priority" of an item (ascii value - offset)
 fn priority(c: char) -> u32 {
@@ -28,65 +30,33 @@ fn string_to_hashset(s: &str) -> HashSet<char> {
     set
 }
 
-fn find_common_character(s1: &str, s2: &str, s3: &str) -> char {
-    let m1 = string_to_hashset(s1);
-    let m2 = string_to_hashset(s2);
-    let m3 = string_to_hashset(s3);
-
+fn find_common_character(lines: Vec<&str>) -> char {
     // TODO: look into why .copied() is necessary here
     // https://users.rust-lang.org/t/intersection-of-hashsets/32351
-    let sect1: HashSet<char> = m1.intersection(&m2).copied().collect();
-    let sect2: Vec<char> = sect1.intersection(&m3).copied().collect();
-
-    sect2[0]
-}
-
-fn parse_rucksacks<R: BufRead>(reader: &mut R) -> Vec<char> {
-    let mut badges: Vec<char> = Vec::new();
-    let mut group: [&str; 3];
-
-    for (i, line) in reader.lines().enumerate() {
-        let contents = match line {
-            Ok(x) => x,
-            Err(_) => continue,
-        };
-
-        let idx = i % 3;
-
-        group[idx] = &contents;
-
-        let badge = find_common_character(group[0], group[1], group[2]);
-        badges.push(badge);
-    }
-
-    badges
+    lines
+        .into_iter()
+        .map(string_to_hashset)
+        .reduce(|x, y| x.intersection(&y).copied().collect())
+        .unwrap()
+        .drain()
+        .next()
+        .unwrap()
 }
 
 fn main() {
     let mut reader = io::stdin().lock();
-    let badges = parse_rucksacks(&mut reader);
 
-    let mut total_priority = 0;
-    for badge in badges {
-        total_priority += priority(badge);
-    }
-
-    println!("Total priority: {}", total_priority);
+    let common = reader
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(find_common_character)
+        .next();
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    // #[test]
-    // fn test_string_to_hashmap() {
-    //     let mut map = string_to_hashmap("ada");
-
-    //     // TODO: look up idiomatic way to get values out of hashmap,
-    //     // i.e. .entry() vs .get()
-    //     assert_eq!(*map.entry('a').or_default(), 2);
-    //     assert_eq!(*map.entry('d').or_default(), 1);
-    // }
 
     #[test]
     fn test_priority() {
@@ -97,7 +67,16 @@ mod tests {
     }
 
     #[test]
+    fn test_string_to_hashset() {
+        let mut s = HashSet::new();
+        s.insert('c');
+        s.insert('b');
+
+        assert_eq!(string_to_hashset(&"bcbccb"), s);
+    }
+
+    #[test]
     fn test_find_common_character() {
-        assert_eq!(find_common_character("s1", "s2", "s3"), 's');
+        assert_eq!(find_common_character(vec!["s1", "s2", "s3"]), 's');
     }
 }
